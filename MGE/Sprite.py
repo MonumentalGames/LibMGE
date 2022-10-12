b9f04fa1e23298464df34ccbd474ad30fa0d02d3
Lucas Guimarães <78770576+lucas224112@users.noreply.github.com>
2022-10-12 05:25:19 -0300

    Delete MGE directory

--- MGE/Sprite.py
+++
@@ -1,9 +0,0 @@
-
-class Sprite:
-    def __init__(self, texture="", localization=(0, 0), size=(0, 0)):
-        self.texture = texture
-        self.localization = localization
-        self.size = size
-
-    def get_size_localization(self):
-        return [self.size, self.localization]
