b2789b89599025d311005a220cc1074d37e98d03
Lucas Guimarães <lucasguimaraes2421@gmail.com>
2024-10-01 06:02:18 -0300

    Update Log.py

--- MGE/Log.py
+++ MGE/Log.py
@@ -1,33 +1,35 @@
+from typing import NoReturn
 
 __all__ = ["Log", "LogDebug", "LogInfo", "LogWarn", "LogError", "LogCritical"]
 
-reset = "\033[0m"
-gray = "\033[90m"
-red = "\033[91m"
-green = "\033[92m"
-yellow = "\033[93m"
-blue = "\033[94m"
-purple = "\033[95m"
-cyan = "\033[96m"
-white = "\033[97m"
+class _ConsoleColors:
+    Reset = "\033[0m"
+    Gray = "\033[90m"
+    Red = "\033[91m"
+    Green = "\033[92m"
+    Yellow = "\033[93m"
+    Blue = "\033[94m"
+    Purple = "\033[95m"
+    Cyan = "\033[96m"
+    White = "\033[97m"
 
 def Log(msg=""):
-    print(f"{gray}{msg}{reset}")
+    print(f"{_ConsoleColors.Gray}{msg}{_ConsoleColors.Reset}")
 
 def LogDebug(msg=""):
-    print(f"{yellow}Debug{reset}: {msg}")
+    print(f"{_ConsoleColors.Yellow}Debug{_ConsoleColors.Reset}: {msg}")
 
 def LogInfo(msg=""):
-    print(f"{green}Info{reset}: {gray}{msg}{reset}")
+    print(f"{_ConsoleColors.Green}Info{_ConsoleColors.Reset}: {_ConsoleColors.Gray}{msg}{_ConsoleColors.Reset}")
 
 def LogWarn(msg=""):
-    print(f"{yellow}Warning{reset}: {msg}")
+    print(f"{_ConsoleColors.Yellow}Warning{_ConsoleColors.Reset}: {msg}")
 
 def LogError(msg=""):
-    print(f"{red}Error{reset}: {msg}")
+    print(f"{_ConsoleColors.Red}Error{_ConsoleColors.Reset}: {msg}")
 
-def LogCritical(msg="", long_msg="", exit_code=1):
-    print(f"{red}Critical Error{reset}: {msg}")
+def LogCritical(msg="", long_msg="", exit_code=1) -> NoReturn:
+    print(f"{_ConsoleColors.Red}Critical Error{_ConsoleColors.Reset}: {msg}")
     if long_msg:
         Log(long_msg)
     raise SystemExit(exit_code)
